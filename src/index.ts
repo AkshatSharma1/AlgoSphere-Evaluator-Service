f176ecdc19f96ee988a80016ed2d3a11e7bcd6e1
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-07-26 17:10:19 +0530

    API structure setup done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Express } from 'express';
 
 import serverConfig from './config/serverConfig';
+import apiRouter from './routes';
 
 const app: Express = express();
 
+app.use('/api', apiRouter);
+
 app.listen(serverConfig.PORT, () => {
-  const s: number = 10;
-  console.log(s);
   console.log(`Server started at PORT ${serverConfig.PORT}`);
 });
